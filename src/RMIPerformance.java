9d64784723f58f59084c3ccd62eb525e980486fd
Petre Stegaroiu <2082041S@student.gla.ac.uk>
2016-11-21 15:24:21 +0000

    RMIPerformance generates barcharts

--- src/RMIPerformance.java
+++ src/RMIPerformance.java
@@ -1,30 +1,30 @@
 
 
-import java.net.MalformedURLException;
+import java.util.*;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Steghi
- */
-public class RMIPerformance 
+public class RMIPerformance extends Application
 {
 
+
+    private static Map<Float, Long> countPerMS = new HashMap<>();
     private static AuctionSystem auctionSystem = null;
-    private static void RMIPerformance(String methodCall, long iterations) throws RemoteException
+    private static String method = "";
+    private static float callClientMethod(String methodCall, long iterations) throws RemoteException
     {
         long startTime = System.currentTimeMillis();
         long auctionID = 0;
+        
         Client client = null;
         switch (methodCall)
         {
@@ -59,9 +59,10 @@ public class RMIPerformance
         
 
         long elapsedTime = System.currentTimeMillis() - startTime;
-        System.out.format(iterations + " " + methodCall +" calls in %d ms - %.02f ms/" + methodCall + "\n",
+        System.out.format(iterations + " " + methodCall +" calls in %d ms - %.03f ms/" + methodCall + "\n",
                     elapsedTime, (float)elapsedTime/iterations);
         System.out.println();
+        return (float)elapsedTime/iterations;
     }
         
         
@@ -69,16 +70,56 @@ public class RMIPerformance
     { 
         try 
         {
+            method = args[0];
+            long iterations = Long.parseLong(args[1]);
             AuctionSystemImpl ai = new AuctionSystemImpl();
             auctionSystem = (AuctionSystem) UnicastRemoteObject.exportObject(ai, 0);
             Naming.rebind("rmi://localhost:" + "1099" + "/AuctionService", auctionSystem);
-            RMIPerformance(args[0], Long.parseLong(args[1]));
+            for (int i = 0; i< 100; i++)
+            {
+                float msPerCall = callClientMethod(method, iterations );
+                if (countPerMS.containsKey(msPerCall))
+                {
+                    long count = countPerMS.get(msPerCall);
+                    countPerMS.put(msPerCall, count+1);
+                }
+                else
+                {
+                    countPerMS.put(msPerCall, 1L);
+                }
+            }             
         } 
         catch (Exception ex) 
         {
-            System.out.println("Failed to connect to Server");
-        }
-        System.exit(0);
-        
+            System.out.println();
+            System.out.println(ex);
+            System.exit(0);
+        }    
+        launch(args);        
     }
+
+    @Override
+    public void start(Stage stage) throws Exception 
+    {
+        stage.setTitle(method + " calls per milisecond bar chart");
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> bc = new BarChart<>(xAxis,yAxis);
+        bc.setTitle(method +" calls per milisecond bar chart");
+        xAxis.setLabel("Milisecond");       
+        yAxis.setLabel(method + " calls");
+        
+        XYChart.Series serie = new XYChart.Series();
+        SortedSet<Float> keys = new TreeSet<>(countPerMS.keySet());
+        for (Float key: keys)
+        {
+            serie.getData().add(new XYChart.Data(key.toString(), countPerMS.get(key)));
+            
+        }
+        Scene scene  = new Scene(bc,800,600);
+        bc.getData().add(serie);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
